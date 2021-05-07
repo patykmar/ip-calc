3816a0cc884ffa93663e937001c974df4d3f5611
patykmar <patyk.m@gmail.com>
2021-05-07 23:48:13 +0200

    Rewrite comment, removed method setCidr()

--- src/Entity/IP/v4/Ipv4netmask.php
+++ src/Entity/IP/v4/Ipv4netmask.php
@@ -1,14 +1,9 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace App\Entity\IP\v4;
 
-namespace App\Classes\IP\v4;
 
-use App\Classes\IP\Model\Ipv4NetworkModel;
+use App\Model\Ipv4NetworkModel;
 use InvalidArgumentException;
 
 /**
@@ -18,86 +13,94 @@ use InvalidArgumentException;
  */
 class Ipv4netmask extends Ipv4address
 {
-    public const CIDR_RANGE_MIN = 1;
+    /** @const int IPv4 netmask CIDR min int value */
+    public const IPV4_NETMASK_CIDR_VALUE_MIN = 1;
 
-    public const CIDR_RANGE_MAX = 32;
+    /** @const int IPv4 netmask CIDR max int value */
+    public const IPV4_NETMASK_CIDR_VALUE_MAX = 32;
 
-   /** @var int network mask in CIDR notation */
-   private $cidr;
+    /** @var int network mask in CIDR notation */
+    private int $cidr;
 
-   /** @var string Wildcard mask */
-   private $wildcard;
+    /** @var string Wildcard mask */
+    private string $wildcard;
 
-   /** @var int Wildcard mask integer */
-   private $wildcardInt;
+    /** @var int Wildcard mask integer */
+    private int $wildcardInt;
 
-   public function __construct(int $cidr = 24)
-   {
-      if (filter_var($cidr, FILTER_VALIDATE_INT,
-                      ['options' => array(
-                              'min_range' => self::CIDR_RANGE_MIN,
-                              'max_range' => self::CIDR_RANGE_MAX,
-                  )])) {
-         $this->setCidr($cidr);
-         parent::__construct($this->getDecadic());
-         $this->setWildcard();
-         $this->setWildcardInt();
-      } else {
-         throw new InvalidArgumentException("Netmask is in wrong range");
-      }
-   }
-
-   /** @param int $cidr CIDR format of netmask 1 - 32 */
-   private function setCidr(int $cidr): void
-   {
-      $this->cidr = $cidr;
-   }
-
-   /** Set network wildcard based on network binary format */
-   private function setWildcard(): void
-   {
-      $out_local = "";
-      $netMaskBin_local = $this->getBinary();
-      for ($i = 0; $i < strlen($netMaskBin_local); $i++) {
-         if ($netMaskBin_local[$i] == '0') {
-            $out_local .= '1';
-         } else {
-            $out_local .= '0';
-         }
-      }
-      $this->wildcard = $out_local;
-   }
-
-   /** Set network wildcard as integer number */
-   private function setWildcardInt(): void
-   {
-      $this->wildcardInt = bindec($this->getWildcard());
-   }
+    public function __construct(int $cidr = 24)
+    {
+        if (filter_var($cidr, FILTER_VALIDATE_INT,
+            ['options' => array(
+                'min_range' => self::IPV4_NETMASK_CIDR_VALUE_MIN,
+                'max_range' => self::IPV4_NETMASK_CIDR_VALUE_MAX,
+            )])) {
+            $this->cidr = $cidr;
+            parent::__construct($this->getDecadic());
+            $this->setWildcard()
+                ->setWildcardInt();
+        } else {
+            throw new InvalidArgumentException("Netmask is in wrong range");
+        }
+    }
 
     /**
-    * @return int network mask in CIRD format
-    */
-   public function getCidr(): int
-   {
-      return $this->cidr;
-   }
+     * Set network wildcard based on network binary format
+     * @return self
+     */
+    private function setWildcard(): self
+    {
+        $out_local = "";
+        $netMaskBin_local = $this->getBinary();
+        for ($i = 0; $i < strlen($netMaskBin_local); $i++) {
+            if ($netMaskBin_local[$i] == '0') {
+                $out_local .= '1';
+            } else {
+                $out_local .= '0';
+            }
+        }
+        $this->wildcard = $out_local;
+        return $this;
+    }
 
-   /** @return string network mask in dec format  */
-   public function getDecadic(): string
-   {
-      return Ipv4NetworkModel::CIDR_TO_NETWORK[$this->getCidr()];
-   }
+    /**
+     * Set network wildcard as integer number
+     */
+    private function setWildcardInt(): void
+    {
+        $this->wildcardInt = bindec($this->getWildcard());
+    }
 
-   /** @return string network wildcard in binary */
-   public function getWildcard(): string
-   {
-      return $this->wildcard;
-   }
+    /**
+     * @return int network mask in CIRD format
+     */
+    public function getCidr(): int
+    {
+        return $this->cidr;
+    }
 
-   /** @return int number of wildcard mask */
-   public function getWildcardInt(): int
-   {
-      return $this->wildcardInt;
-   }
+    /**
+     * @return string network mask in dec format
+     */
+    public function getDecadic(): string
+    {
+        return Ipv4NetworkModel::CIDR_TO_NETWORK[$this->getCidr()];
+    }
+
+    /**
+     * @return string network wildcard in binary
+     */
+    public function getWildcard(): string
+    {
+        return $this->wildcard;
+    }
+
+    /**
+     * @return int number of wildcard mask
+     */
+    public function getWildcardInt(): int
+    {
+        return $this->wildcardInt;
+    }
 
 }
