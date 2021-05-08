7ccd24b1a50818ce155dd08f7c01f27226e12473
Martin Patyk <patyk.m@gmail.com>
2021-05-08 15:18:12 +0200

    change namespace, change InvalidArgumentException from Nette to PHP, fix missing return statement in __toString method

--- src/Entity/IP/v6/Ipv6netmask.php
+++ src/Entity/IP/v6/Ipv6netmask.php
@@ -1,10 +1,8 @@
 <?php
 
-declare(strict_types=1);
+namespace App\Entity\IP\v6;
 
-namespace App\IpamModule\Classes;
-
-use Nette;
+use InvalidArgumentException;
 
 /**
  * Description of Ipv6netmask
@@ -14,113 +12,115 @@ use Nette;
 class Ipv6netmask extends Ipv6address
 {
 
-   /** @var int network mask in CIDR notation */
-   private $cidr;
+    /** @var int network mask in CIDR notation */
+    private $cidr;
 
-   /** @var string Wildcard mask */
-   private $wildcard;
+    /** @var string Wildcard mask */
+    private $wildcard;
 
-   public function __construct(int $cidr = 64)
-   {
-      if (filter_var($cidr, FILTER_VALIDATE_INT,
-                      ['options' => array(
-                              'min_range' => 1,
-                              'max_range' => 128,
-                  )])) {
-         $this->setCidr($cidr);
-         $this->cidrToBin();
-         $this->convertNetmaskToWildcard();
-         $this->binToHexa();
-      } else {
-         throw new Nette\InvalidArgumentException("Netmask is in wrong range");
-      }
-   }
 
-   /**
-    * @param int $cidr Integer value of IPv4 netmask CIDR
-    */
-   private function setCidr(int $cidr): void
-   {
-      $this->cidr = $cidr;
-   }
+    public function __construct(int $cidr = 64)
+    {
+        if (filter_var($cidr, FILTER_VALIDATE_INT,
+            ['options' => array(
+                'min_range' => 1,
+                'max_range' => 128,
+            )])) {
+            $this->setCidr($cidr);
+            $this->cidrToBin();
+            $this->convertNetmaskToWildcard();
+            $this->binToHexa();
+            parent::__construct($this->getHexa());
+        } else {
+            throw new InvalidArgumentException("Netmask is in wrong range");
+        }
+    }
 
-   /**
-    * @param string $wildcard Binary representation of netmask
-    */
-   private function setWildcard(string $wildcard): void
-   {
-      $this->wildcard = $wildcard;
-   }
+    /**
+     * @param int $cidr Integer value of IPv4 netmask CIDR
+     */
+    private function setCidr(int $cidr): void
+    {
+        $this->cidr = $cidr;
+    }
 
-   /**
-    * Set binary format of netmask
-    */
-   private function cidrToBin(): void
-   {
-      $binNetmask = "";
-      for ($i = 0; $i < parent::IPV6_BIN_LEN; $i++) {
-         ($this->getCidr() > $i) ? $binNetmask .= "1" : $binNetmask .= "0";
-      }
+    /**
+     * @param string $wildcard Binary representation of netmask
+     */
+    private function setWildcard(string $wildcard): void
+    {
+        $this->wildcard = $wildcard;
+    }
 
-      $this->setBin($binNetmask);
-   }
+    /**
+     * Set binary format of netmask
+     */
+    private function cidrToBin(): void
+    {
+        $binNetmask = "";
+        for ($i = 0; $i < parent::IPV6_BIN_LEN; $i++) {
+            ($this->getCidr() > $i) ? $binNetmask .= "1" : $binNetmask .= "0";
+        }
 
-   private function convertNetmaskToWildcard(): void
-   {
-      $out_local = "";
-      $netMaskBin_local = $this->getBin();
-      for ($i = 0; $i < strlen($netMaskBin_local); $i++) {
-         ($netMaskBin_local[$i] == '0') ? $out_local .= '1' : $out_local .= '0';
-      }
+        $this->setBin($binNetmask);
+    }
 
-      $this->setWildcard($out_local);
-      unset($out_local);
-   }
+    private function convertNetmaskToWildcard(): void
+    {
+        $out_local = "";
+        $netMaskBin_local = $this->getBin();
+        for ($i = 0; $i < strlen($netMaskBin_local); $i++) {
+            ($netMaskBin_local[$i] == '0') ? $out_local .= '1' : $out_local .= '0';
+        }
 
-   /**
-    * Convert binary netmask to hexa format
-    */
-   private function binToHexa(): void
-   {
-      $localString = "";
-      $localArrayBinary = str_split($this->getBin(), parent::HEXTET_BIN_LEN);
-      $localArrayHexa = array();
+        $this->setWildcard($out_local);
+        unset($out_local);
+    }
 
-      foreach ($localArrayBinary as $item) {
-         $localString .= base_convert($item, 2, 16) . ":";
-         $localArrayHexa[] = base_convert($item, 2, 16);
-      }
+    /**
+     * Convert binary netmask to hexa format
+     */
+    private function binToHexa(): void
+    {
+        $localString = "";
+        $localArrayBinary = str_split($this->getBin(), parent::HEXTET_BIN_LEN);
+        $localArrayHexa = array();
 
-      // remove last colon
-      $localString = rtrim($localString, ":");
+        foreach ($localArrayBinary as $item) {
+            $localString .= base_convert($item, 2, 16) . ":";
+            $localArrayHexa[] = base_convert($item, 2, 16);
+        }
 
-      $this->setIpv6Array($localArrayHexa);
-      $this->setHexa($localString);
+        // remove last colon
+        $localString = rtrim($localString, ":");
 
-      unset($localString, $localArrayBinary, $localArrayHexa);
-   }
+        $this->setIpv6Array($localArrayHexa);
+        $this->setHexa($localString);
 
-   /**
-    * @return int Integer value of CIDR number of IPv6 netmask
-    */
-   public function getCidr(): int
-   {
-      return $this->cidr;
-   }
+        unset($localString, $localArrayBinary, $localArrayHexa);
+    }
 
-   /**
-    * @return string binary representation of netmask wildcard
-    */
-   public function getWildcard(): string
-   {
-      return $this->wildcard;
-   }
+    /**
+     * @return int Integer value of CIDR number of IPv6 netmask
+     */
+    public function getCidr(): int
+    {
+        return $this->cidr;
+    }
 
-   /**
-    * @return string Hexa representation of IPv6 netmask
-    */
-   public function __toString(): string
-   {
-      parent::__toString();
-   }
+    /**
+     * @return string binary representation of netmask wildcard
+     */
+    public function getWildcard(): string
+    {
+        return $this->wildcard;
+    }
+
+    /**
+     * @return string Hexa representation of IPv6 netmask
+     */
+    public function __toString(): string
+    {
+        return parent::__toString();
+    }
 }
