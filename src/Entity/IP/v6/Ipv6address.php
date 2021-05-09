51b414468e0b04a2b4b3bdf2563cb469c298fbd2
Martin Patyk <patyk.m@gmail.com>
2021-05-09 18:09:36 +0200

    Export convert function to service class, extend class from IP class

--- src/Entity/IP/v6/Ipv6address.php
+++ src/Entity/IP/v6/Ipv6address.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity\IP\v6;
 
+use App\Entity\IP\Ip;
+use App\Services\Ipv6addressService;
 use InvalidArgumentException;
 
 /**
@@ -9,171 +11,77 @@ use InvalidArgumentException;
  *
  * @author patykmar
  */
-class Ipv6address
+class Ipv6address extends Ip
 {
+    /** @var int binary length of IPv6 address */
+    public const IPV6_BIN_LEN = 128;
 
-   /** @var int binary length of IPv6 address */
-   public const IPV6_BIN_LEN = 128;
+    /** @var int number of bits */
+    public const HEXTET_BIN_LEN = 16;
 
-   /** @var int number of bits */
-   public const HEXTET_BIN_LEN = 16;
+    /** @var int total number of hextet in IPv6 address */
+    public const HEXTET_COUNT = 8;
 
-   /** @var int total number of hextet in IPv6 address */
-   public const HEXTET_COUNT = 8;
+    /** @var string IPv6 address in hexa format */
+    private string $hexa = "";
 
-   /** @var string IPv6 address in hexa format */
-   private $hexa;
+    public function __construct(string $ipv6address = "::1")
+    {
+        if (filter_var($ipv6address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+            $this->hexa = strtolower($ipv6address);
+            $this->setAddressArray();
+            $this->setBinary();
+        } else {
+            throw new InvalidArgumentException("IPv6 is not in valid format");
+        }
 
-   /** @var string IPv6 address in binary format */
-   private $bin;
+        if (strlen($this->binary) != self::IPV6_BIN_LEN) {
+            throw new InvalidArgumentException("Len of IPv6 is out of expected range");
+        }
+    }
 
-   /** @var array() IPv6 each hextet in dedicate item of one array */
-   private $ipv6Array;
+    /**
+     * Set address array
+     * @return self
+     */
+    protected function setAddressArray(): self
+    {
+        $this->addressArray = Ipv6addressService::hexToArray($this->hexa);
+        return $this;
+    }
 
-   public function __construct(string $ipv6address = "::1")
-   {
-      if (filter_var($ipv6address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
-         $this->setHexa(strtolower($ipv6address));
+    /**
+     * Set binary format of IPv6 address.
+     * @return self
+     */
+    protected function setBinary(): self
+    {
+        $this->binary = Ipv6addressService::hexToBin($this->addressArray);
+        return $this;
+    }
 
-         $this->hexToArray();
+    /**
+     * @param string $hexa
+     */
+    public function setHexa(string $hexa): void
+    {
+        $this->hexa = $hexa;
+    }
 
-         $this->hexToBin();
-      } else {
-         throw new InvalidArgumentException("IPv6 is not in valid format");
-      }
 
-      if (strlen($this->bin) != self::IPV6_BIN_LEN)
-         throw new InvalidArgumentException("Len of IPv6 is out of expected range");
-   }
+    /**
+     * @return string Hexa representation of IPv6 address
+     */
+    public function __toString(): string
+    {
+        return $this->getHexa();
+    }
 
-   protected function setHexa(string $ipv6Hexa): void
-   {
-      $this->hexa = $ipv6Hexa;
-   }
-
-   /**
-    * Convert IPv6 address to array splitted by colon
-    */
-   private function hexToArray(): void
-   {
-      // split ipv6 address to array 
-      $ipv6Array = explode(':', $this->hexa);
-
-      // is the IPv6 in shortened representation 
-      if (str_contains($this->hexa, "::")) {
-         // then add missing zeros 
-
-         $ipv6ArrayAddMissingZeros = array();
-         $countOfMissingHextet = self::HEXTET_COUNT - count($ipv6Array);
-
-         foreach ($ipv6Array as $hextet) {
-            if (strlen($hextet) == 0) {
-               for ($i = 0; $i <= $countOfMissingHextet; $i++)
-                  $ipv6ArrayAddMissingZeros[] = "0000";
-
-               // already added missing zeros in to IPv6 adderr, set variable to zero
-               $countOfMissingHextet = 0;
-            } else {
-               $ipv6ArrayAddMissingZeros[] = $hextet;
-            }
-#            if (count($ipv6Array) > self::HEXTET_COUNT)
-#               break;
-         }
-
-         // set the original array with added zeros
-         $this->ipv6Array = $ipv6ArrayAddMissingZeros;
-      } else {
-         $this->ipv6Array = $ipv6Array;
-      }
-
-      // unset local variable
-      unset($ipv6ArrayAddMissingZeros, $ipv6Array);
-   }
-
-   /**
-    * Convert IPv6 address from Hexa to Binary format. Only for internal purpose
-    * @return void 
-    */
-   private function hexToBin(): void
-   {
-      $localBin = "";
-      foreach ($this->ipv6Array as $hextet) {
-         $localBin .= str_pad(base_convert($hextet, 16, 2), self::HEXTET_BIN_LEN, "0", STR_PAD_LEFT);
-      }
-      $this->setBin($localBin);
-   }
-
-   /**
-    * @return string IPv6 address in Hexa format 
-    */
-   public function getHexa(): string
-   {
-      return $this->hexa;
-   }
-
-   /**
-    * @return string IPv6 address in Binary format
-    */
-   public function getBin(): string
-   {
-      return $this->bin;
-   }
-
-   /**
-    * Convert IP from BIN to HEXA
-    * BIN=11000000101010000000000100000001 to 192.168.1.1
-    * @param string $inputIP IPv4Address in binary format
-    * @return Ipv6address New object of Ipv4address
-    */
-   public static function binToDec(string $inputIP): Ipv6address
-   {
-      // input must be only in binary format and length must be 128 bits
-      if (filter_var($inputIP, FILTER_VALIDATE_REGEXP,
-                      array(
-                          "options" => array(
-                              "regexp" => '/[0,1]/'
-                  )))
-              AND
-              filter_var(strlen($inputIP), FILTER_VALIDATE_INT, array(
-                  'options' => array(
-                      'min_range' => self::IPV6_BIN_LEN,
-                      'max_range' => self::IPV6_BIN_LEN
-          )))) {
-
-         $inputIpArray = str_split($inputIP, self::HEXTET_BIN_LEN);
-         $ipOutput = "";
-         foreach ($inputIpArray as $hextet){
-            $ipOutput .= base_convert($hextet, 2, 16). ":";
-         }
-
-         #dump($ipOutput);
-         #dump(substr($ipOutput, 0, -1));
-         #exit;
-         return new Ipv6address(substr($ipOutput, 0, -1));
-      } else {
-         throw new InvalidArgumentException("Input is not in valid format");
-      }
-   }
-
-   /**
-    * @param string $binary binary format of IPv6 address
-    */
-   protected function setBin(string $binary): void
-   {
-      $this->bin = $binary;
-   }
-
-   protected function setIpv6Array(array $ipv6Array): void
-   {
-      $this->ipv6Array = $ipv6Array;
-   }
-
-   /**
-    * @return string Hexa representation of IPv6 address
-    */
-   public function __toString(): string
-   {
-      return $this->getHexa();
-   }
-
+    /**
+     * @return string IPv6 address in Hexa format
+     */
+    public function getHexa(): string
+    {
+        return $this->hexa;
+    }
 }
