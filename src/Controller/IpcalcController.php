48e838a75402d6bcfd337a648dc598a677dfd4e7
Martin Patyk <patyk.m@gmail.com>
2021-05-09 20:54:01 +0200

    rewrite versionSixCalc() function for rendering information regarding IPv6 subnet, fix IPv4 subnet when exception handling

--- src/Controller/IpcalcController.php
+++ src/Controller/IpcalcController.php
@@ -8,6 +8,7 @@ use InvalidArgumentException;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 
 class IpcalcController extends AbstractController
@@ -34,29 +35,30 @@ class IpcalcController extends AbstractController
             $ipv4Subnet = new Ipv4subnet($ip);
         } catch (InvalidArgumentException $exception) {
             $this->logger->error($exception->getMessage());
-            $ipv4Subnet = "192.168.2.0/24";
+            $ipv4Subnet = new Ipv4subnet("192.168.2.0/24");
         }
 
-
-        return $this->render('ipcalc/index.html.twig', [
+        return $this->render('ipcalc/ipv4.html.twig', [
             'subnet' => $ipv4Subnet,
         ]);
     }
 
     /**
-     * @Route("/ipv6calc/{ip}", name="ipv6calc", requirements={"ip"=".+"}, defaults={"192.168.1.0/24"})
+     * @Route("/ipv6calc/{ip}", name="ipv6calc", requirements={"ip"=".+"}, defaults={"abc::/64"})
      * @param string $ip
      * @return Response
      */
     public function versionSixCalc(string $ip = "abc::/64")
     {
-        $ipv6Subnet = new Ipv6subnet($ip);
-        dump($ip);
-        dd($ipv6Subnet);
+        try {
+            $ipv6Subnet = new Ipv6subnet($ip);
+        } catch (InvalidArgumentException $exception) {
+            $this->logger->error($exception->getMessage());
+            $ipv6Subnet = new Ipv6subnet("abc::/64");
+        }
 
-
-        return $this->render('ipcalc/index.html.twig', [
-            'subnet' => $ip,
+        return $this->render('ipcalc/ipv6.html.twig', [
+            'subnet' => $ipv6Subnet,
         ]);
     }
 }
