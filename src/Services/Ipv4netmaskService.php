d45de4471cd39850450614c59c630a342e578008
patykmar <patyk.m@gmail.com>
2021-05-10 11:17:03 +0200

    created Ipv4netmaskService and implement first method which is converting netmask to wildcard

---
+++ src/Services/Ipv4netmaskService.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Services;
+
+
+class Ipv4netmaskService
+{
+    public static function convertNetmaskToWildcard(string $binaryNetmask): string
+    {
+        $out_local = "";
+        $netMaskBin_local = $binaryNetmask;
+        for ($i = 0; $i < strlen($netMaskBin_local); $i++) {
+            ($netMaskBin_local[$i] == '0') ? $out_local .= '1' : $out_local .= '0';
+        }
+        return $out_local;
+    }
+}
