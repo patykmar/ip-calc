e7a4a1973b3842133216c50a17ee4a45cdf23f59
Martin Patyk <patyk.m@gmail.com>
2021-05-16 10:56:13 +0200

    added api functions for ipv4 and ipv6

--- src/Controller/IpcalcController.php
+++ src/Controller/IpcalcController.php
@@ -25,7 +25,7 @@ class IpcalcController extends AbstractController
 
 
     /**
-     * @Route("/ipv4calc/{ip}", name="ipv4calc", requirements={"ip"=".+"}, defaults={"192.168.1.0/24"})
+     * @Route("ipv4calc/{ip}", name="ipv4calc", requirements={"ip"=".+"}, defaults={"192.168.1.0/24"})
      * @param string $ip
      * @return Response
      */
@@ -40,6 +40,32 @@ class IpcalcController extends AbstractController
 
         return $this->render('ipcalc/ipv4.html.twig', [
             'subnet' => $ipv4Subnet,
+        ]);
+    }
+
+    /**
+     * @Route("api/ipv4calc/{ip}", name="apiIpv4calc", requirements={"ip"=".+"}, defaults={"192.168.1.0/24"})
+     * @param string $ip
+     * @return Response
+     */
+    public function apiVersionFourCalc(string $ip = "192.168.1.0/24"): Response
+    {
+        try {
+            $ipv4Subnet = new Ipv4subnet($ip);
+        } catch (InvalidArgumentException $exception) {
+            $this->logger->error($exception->getMessage());
+            $ipv4Subnet = new Ipv4subnet("192.168.2.0/24");
+        }
+        return $this->json([
+            'network-subnet' => $ipv4Subnet->ipv4AddressNetwork->getDecadic() . "/" . $ipv4Subnet->ipv4Netmask->getCidr(),
+            'netmask' => $ipv4Subnet->ipv4Netmask->getDecadic(),
+            'network-address' => $ipv4Subnet->ipv4AddressNetwork->getDecadic(),
+            'first-address' => $ipv4Subnet->ipv4FirstAddress->getDecadic(),
+            'last-address' => $ipv4Subnet->ipv4LastAddress->getDecadic(),
+            'broadcast-address' => $ipv4Subnet->ipv4AddressBroadcast->getDecadic(),
+            'number-of-usable-address' => $ipv4Subnet->ipv4LastAddress->getInteger() - $ipv4Subnet->ipv4FirstAddress->getInteger() + 1,
+            'nsx-cidr' => $ipv4Subnet->ipv4FirstAddress->getDecadic() . '/' . $ipv4Subnet->ipv4Netmask->getCidr(),
+            'nsx-static-ip-pool' => $ipv4Subnet->ipv4SecondAddress->getDecadic() . '-' . $ipv4Subnet->ipv4LastAddress->getDecadic(),
         ]);
     }
 
@@ -59,6 +85,29 @@ class IpcalcController extends AbstractController
 
         return $this->render('ipcalc/ipv6.html.twig', [
             'subnet' => $ipv6Subnet,
+        ]);
+    }
+
+    /**
+     * @Route("/api/ipv6calc/{ip}", name="apiIpv6calc", requirements={"ip"=".+"}, defaults={"abc::/64"})
+     * @param string $ip
+     * @return Response
+     */
+    public function apiVersionSixCalc(string $ip = "abc::/64")
+    {
+        try {
+            $ipv6Subnet = new Ipv6subnet($ip);
+        } catch (InvalidArgumentException $exception) {
+            $this->logger->error($exception->getMessage());
+            $ipv6Subnet = new Ipv6subnet("abc::/64");
+        }
+
+        return $this->json([
+            'lookup-address' => $ipv6Subnet->ipv6Address->getHexa() . "/" . $ipv6Subnet->ipv6Netmask->getCidr(),
+            'network-subnet' => $ipv6Subnet->ipv6NetworkAddress->getHexa() . '/' . $ipv6Subnet->ipv6Netmask->getCidr(),
+            'netmask' => $ipv6Subnet->ipv6Netmask->getHexa(),
+            'network-address' => $ipv6Subnet->ipv6NetworkAddress->getHexa(),
+            'last-address' => $ipv6Subnet->ipv6LastAddress->getHexa(),
         ]);
     }
 }
