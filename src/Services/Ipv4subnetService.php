4836e96b08c0224b05e2560ea6e180faeb9e3e79
patykmar <patyk.m@gmail.com>
2021-05-22 18:38:53 +0200

    implemented prepareJsonResponse()

--- src/Services/Ipv4subnetService.php
+++ src/Services/Ipv4subnetService.php
@@ -11,15 +11,49 @@ use InvalidArgumentException;
 
 class Ipv4subnetService
 {
-    private Ipv4subnet $ipv4subnet;
-
     /**
-     * Ipv4subnetService constructor.
-     * @param Ipv4subnet $ipv4subnet
+     * Based on IPv4 Subnet return array for JSON format
      */
-    public function __construct(Ipv4subnet $ipv4subnet)
+    public function prepareJsonResponse(Ipv4subnet $ipv4subnet): array
     {
-        $this->ipv4subnet = $ipv4subnet;
+        return [
+            'network-subnet' => [
+                'key' => 'Network subnet:',
+                'value' => $ipv4subnet->ipv4AddressNetwork->getDecadic() . "/" . $ipv4subnet->ipv4Netmask->getCidr()
+            ],
+            'netmask' => [
+                'key' => 'Netmask:',
+                'value' => $ipv4subnet->ipv4Netmask->getDecadic()
+            ],
+            'network-address' => [
+                'key' => 'Network address:',
+                'value' => $ipv4subnet->ipv4AddressNetwork->getDecadic()
+            ],
+            'first-address' => [
+                'key' => 'First address:',
+                'value' => $ipv4subnet->ipv4FirstAddress->getDecadic()
+            ],
+            'last-address' => [
+                'key' => 'Last address:',
+                'value' => $ipv4subnet->ipv4LastAddress->getDecadic()
+            ],
+            'broadcast-address' => [
+                'key' => 'Broadcast address:',
+                'value' => $ipv4subnet->ipv4AddressBroadcast->getDecadic()
+            ],
+            'number-of-usable-address' => [
+                'key' => 'Number of usable address:',
+                'value' => $ipv4subnet->ipv4LastAddress->getInteger() - $ipv4subnet->ipv4FirstAddress->getInteger() + 1
+            ],
+            'nsx-cidr' => [
+                'key' => 'NSX CIDR:',
+                'value' => $ipv4subnet->ipv4FirstAddress->getDecadic() . '/' . $ipv4subnet->ipv4Netmask->getCidr()
+            ],
+            'nsx-static-ip-pool' => [
+                'key' => 'NSX Static IP pool:',
+                'value' => $ipv4subnet->ipv4SecondAddress->getDecadic() . '-' . $ipv4subnet->ipv4LastAddress->getDecadic()
+            ],
+        ];
     }
 
 
@@ -49,7 +83,7 @@ class Ipv4subnetService
 
             for ($i = 1; $i < $countOfLoop; $i++) {
                 $networkAddress = $smallerWildcard * $i;
-                $newIpAddressObject = Ipv4addressFactory::add($this->ipv4subnet->ipv4AddressNetwork,$networkAddress);
+                $newIpAddressObject = Ipv4addressFactory::add($this->ipv4subnet->ipv4AddressNetwork, $networkAddress);
                 $subnet = $newIpAddressObject->getDecadic() . '/' . $smallerNetwork->getCidr();
                 $returnArray[] = new Ipv4subnet($subnet);
                 unset($subnet, $newIpAddressObject, $networkAddress);
