427d5a6278d88e70c23cb1a8607c31a6a0bf87f9
patykmar <patyk.m@gmail.com>
2021-05-25 08:43:26 +0200

    re-format file

--- frontend/src/components/ipcalc/resultsTable/SubnetTableRow.js
+++ frontend/src/components/ipcalc/resultsTable/SubnetTableRow.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default class SubnetTableRow extends React.Component {
 
-  render = () => {
-    return (
-      <tr>
-        <th scope="row">{this.props.rowKey}</th>
-        <td>{this.props.rowValue}</td>
-      </tr>
-    );
-  };
+    render = () => {
+        return (
+            <tr>
+                <th scope="row">{this.props.rowKey}</th>
+                <td>{this.props.rowValue}</td>
+            </tr>
+        );
+    };
 }
