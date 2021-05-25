07145621f669563d10755f7839a7e90936bdbca7
patykmar <patyk.m@gmail.com>
2021-05-25 08:43:40 +0200

    removed logging

--- frontend/src/components/ipcalc/resultsTable/SubnetTableV6.js
+++ frontend/src/components/ipcalc/resultsTable/SubnetTableV6.js
@@ -3,11 +3,6 @@ import SubnetTableRow from "./SubnetTableRow";
 
 export default class SubnetTableV4 extends React.Component {
 
-    componentDidMount() {
-        console.log("SubnetTableV4 - componentDidMount");
-        console.log(this.props.outputTableData);
-    }
-
     render = () => {
         return (
             <div className="col-lg-7 text-center text-lg-start">
